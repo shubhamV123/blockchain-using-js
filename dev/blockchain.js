f163a17677c0a530cef156a38cbf0558dd853b37
Shubham <shubham@supply.ai>
2019-04-23 01:27:42 +0530

    Added createNewBlock and getLastBlock prototype

---
+++ dev/blockchain.js
@@ -0,0 +1,28 @@
+function BlockChain() {
+    this.chain = []; //All of the blocks that we mine will be stored in this particular array as a chain
+    this.newTransactions = []; //All of the new transactions that are created before they are placed into a block.
+}
+BlockChain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
+    /*
+     * hash and previousBlockHash are both hashes. The only difference is that the hash property deals with the data of the current block, and, the previousBlockHash property deals with the hashing of the data of the previous block.  
+     * This is how you create a new block,
+     * and this is what every block in our blockchain will look like.
+     */
+    const newBlock = {//new block inside of our BlockChain
+        index: this.chain.length + 1, //index value will basically be the block number,
+        timestamp: Date.now(),
+        transactions: this.newTransactions,//all of the new transactions or the pending transactions that have just been created into the new block 
+        nonce: nonce, // nonce is pretty much proof that we've created this new block in a legitimate way by using a proofOfWork method.
+        hash: hash,// all of our transactions are going to be compressed into a single string of code, which will be our hash.
+        previousBlockHash: previousBlockHash // data from our previous block or the previous block to the current block hashed into a string.
+    };
+    this.newTransactions = [];  //clear out the entire new transactions array so that we can start over for the next block and already stored in newBlock
+    this.chain.push(newBlock);
+
+    return newBlock;
+
+}
+BlockChain.prototype.getLastBlock = function () {
+    return this.chain[this.chain.length - 1];
+}
+module.exports = BlockChain;
---
+++ dev/test.js
@@ -0,0 +1,8 @@
+const BlockChain = require('./blockchain');
+
+const bitcoin = new BlockChain();
+bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
+bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
+bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
+bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
+console.log(bitcoin);
