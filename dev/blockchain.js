9e7797ac3774df3821c118869e5fe5881b99e037
Shubham <shubham@supply.ai>
2019-04-24 01:56:40 +0530

    added newTransaction prototype to add transaction into pendingTransaction block

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,6 @@
 function BlockChain() {
     this.chain = []; //All of the blocks that we mine will be stored in this particular array as a chain
-    this.newTransactions = []; //All of the new transactions that are created before they are placed into a block.
+    this.pendingTransaction = []; //All of the new transactions that are created before they are placed into a block.
 }
 BlockChain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
     /*
@@ -11,12 +11,12 @@ BlockChain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) 
     const newBlock = {//new block inside of our BlockChain
         index: this.chain.length + 1, //index value will basically be the block number,
         timestamp: Date.now(),
-        transactions: this.newTransactions,//all of the new transactions or the pending transactions that have just been created into the new block 
+        transactions: this.pendingTransaction,//all of the new transactions or the pending transactions that have just been created into the new block 
         nonce: nonce, // nonce is pretty much proof that we've created this new block in a legitimate way by using a proofOfWork method.
         hash: hash,// all of our transactions are going to be compressed into a single string of code, which will be our hash.
         previousBlockHash: previousBlockHash // data from our previous block or the previous block to the current block hashed into a string.
     };
-    this.newTransactions = [];  //clear out the entire new transactions array so that we can start over for the next block and already stored in newBlock
+    this.pendingTransaction = [];  //clear out the entire new transactions array so that we can start over for the next block and already stored in newBlock
     this.chain.push(newBlock);
 
     return newBlock;
@@ -24,5 +24,14 @@ BlockChain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) 
 }
 BlockChain.prototype.getLastBlock = function () {
     return this.chain[this.chain.length - 1];
+}
+BlockChain.prototype.createNewTransaction = function (amount, sender, recipient) {
+    const newTransaction = {
+        amount,
+        sender,
+        recipient
+    }
+    this.pendingTransaction.push(newTransaction);
+    return this.getLastBlock()['index'] + 1
 }
 module.exports = BlockChain;
