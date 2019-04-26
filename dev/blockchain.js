4af9eb908c7ccbfe784a4be23b1a74e1301b38e7
Shubham <shubham@supply.ai>
2019-04-26 22:34:41 +0530

    wip

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,12 +1,20 @@
+const sha256 = require('sha256');
+
+/*
+Blockchain is pretty much a list of blocks.Every single block has to be created and added to chain
+.Proof of Work ensure that our blockchain are secure.
+ */
 function BlockChain() {
     this.chain = []; //All of the blocks that we mine will be stored in this particular array as a chain
     this.pendingTransaction = []; //All of the new transactions that are created before they are placed into a block.
+    this.createNewBlock(100, '0', '0');// first block in any blockchain also known as genesis block
 }
 BlockChain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
     /*
      * hash and previousBlockHash are both hashes. The only difference is that the hash property deals with the data of the current block, and, the previousBlockHash property deals with the hashing of the data of the previous block.  
      * This is how you create a new block,
      * and this is what every block in our blockchain will look like.
+
      */
     const newBlock = {//new block inside of our BlockChain
         index: this.chain.length + 1, //index value will basically be the block number,
@@ -33,5 +41,26 @@ BlockChain.prototype.createNewTransaction = function (amount, sender, recipient)
     }
     this.pendingTransaction.push(newTransaction);
     return this.getLastBlock()['index'] + 1
+}
+//For generation hash function we are going to use sha256
+//currentBlockData simply array of transcations present in this block
+BlockChain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce) {
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
+
+}
+/*
+It takes a lot of work to generate a proof of work, but it is very easy to verify that it is correct.
+*/
+BlockChain.prototype.proofOfWork = function (previousBlockHash, currentBlockData) {
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData,
+        nonce);
+    while (hash.substring(0, 4) !== '0000') {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+    return nonce;
 }
 module.exports = BlockChain;
--- dev/test.js
+++ dev/test.js
@@ -1,16 +1,38 @@
 const BlockChain = require('./blockchain');
 
 const bitcoin = new BlockChain();
-bitcoin.createNewBlock(789457, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56');
+console.log(bitcoin);
 
-bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+// bitcoin.createNewBlock(789457, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56');
 
-bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WPLS214R7T6SJ3G2');
+// bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
 
-bitcoin.createNewTransaction(50, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
-bitcoin.createNewTransaction(200, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
-bitcoin.createNewTransaction(300, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
-bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WPLS214R7T6SJ3G2');
+// bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WPLS214R7T6SJ3G2');
+
+// bitcoin.createNewTransaction(50, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+// bitcoin.createNewTransaction(200, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+// bitcoin.createNewTransaction(300, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+// bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WPLS214R7T6SJ3G2');
+// const previousBlockHash = '87765DA6CCF0668238C1D27C35692E11';
+// const currentBlockData = [
+//     {
+//         amount: 10,
+//         sender: 'B4CEE9C0E5CD571',
+//         recipient: '3A3F6E462D48E8',
+//     },
+//     {
+//         amount: 100,
+//         sender: 'B4CWE9C0E5CD571',
+//         recipient: '3A3F6E462D48E9',
+//     },
+//     {
+//         amount: 10,
+//         sender: 'B4CEEFC0E5CD571',
+//         recipient: '3A3J6E462D48E9',
+//     }
+// ];
+// const nonce = 100;
+// // bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
 
 
-console.log(bitcoin.chain);
+// console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
